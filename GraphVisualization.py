1538cdcb754c90d318ac1ee41d7cfe9707f6c6c2
ruteee <rute.s.abreu@gmail.com>
2019-03-23 11:33:10 -0300

    Implementation of graph visualization for Transfer Entropy dataframe - Python script and notebook available.

---
+++ GraphVisualization.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from graphviz import Digraph
+
+def graph_simple(df, eng = 'dot'):
+    edge_style = ""
+    g = Digraph(engine=eng)
+    in_graph = []
+    for k, row in enumerate(df.index):
+        if any(df.loc[row]):
+            g.node(str(row),row, shape='oval', fontsize='10', width='0', style='filled', fillcolor='#c9c9c9', color="gray")
+            in_graph.append(row)
+
+              
+    for c, col in enumerate(df.columns):
+        if any(df[col]):
+            if col not in in_graph:
+                g.node(str(col), col, shape='oval', fontsize='10', width='0', style='filled', fillcolor='#c9c9c9', color="gray") 
+
+    for j, col in enumerate(df.columns):
+        for i, row in enumerate(df.index):
+            if(df[col][i]):
+                g.edge(str(row), str(col), label=str(df.at[row,col]), style= edge_style, color='black')  
+    return g 
+
